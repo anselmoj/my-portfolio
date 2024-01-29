7a4a9993c54720b646ca41e893ddfb083c70d550
jR <juniorpr.comp@gmail.com>
2024-01-29 13:34:04 -0300

    fix: correcoes

--- profile/src/app/page.js
+++ profile/src/app/page.js
@@ -1,7 +1,15 @@
+import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 
 export default function App() {
   return (
+    <>
+    <Header />
+    <main className='main'>
     <Home />
+
+    </main>
+
+    </>
   );
 }
